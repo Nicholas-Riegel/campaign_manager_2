842a04a7b103343a177944e7570df1d6743fe2cc
Nicholas <nicholaspriegel@gmail.com>
2024-05-22 14:23:22 -0400

    routes complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,65 @@
+import { useState, useEffect } from "react"
+import * as campaignService from './services/campaignService'
+import Nav from "./components/Nav/Nav"
+import Home from './components/Home/Home'
+import Create from './components/Create/Create'
+import Show from './components/Show/Show'
+import Update from "./components/Udate/Update"
 
 function App() {
+
+  const [campaignList, setCampaignList] = useState([])
+  const [page, setPage] = useState('home')
+  const [selectedCampaign, setSelectedCampaign] = useState({})
+
+  const getAllCampaigns = async () => {
+    const allCampaigns = await campaignService.index()
+    setCampaignList(allCampaigns)
+  }
+
+  useEffect(()=>{
+    getAllCampaigns()
+  }, [])
+
+  const selectPage = (page) => {
+    setPage(page)   
+  }
+
+  const handleCreate = async (campaign) => {
+    await campaignService.create(campaign)
+    getAllCampaigns()
+    setPage('home')
+  }
+
+  const handleSelectCampaign = (selection) => {
+    setSelectedCampaign(selection)
+    setPage('show')  
+  }
+
+  const handleDelete = async (campaignId) => {
+    await campaignService.deleteCampaign(campaignId)
+    await getAllCampaigns()
+    setPage('home')
+  }
+
+  const handlePageSelection = (page) => {
+    setPage(page)
+  }
+
+  const handleUpdate = async (campaign) => {
+    await campaignService.updateCampaign(campaign, campaign._id)
+    await getAllCampaigns()
+    setPage('home')
+  }
+
   return (
     <>
-      hello world
+      <Nav {...{selectPage}}/>
+      <h1>Campaign Management System</h1>
+      {page === 'home' && <Home {...{campaignList, handleSelectCampaign}}/>}
+      {page === 'create' && <Create {...{handleCreate}}/>}
+      {page === 'show' && <Show {...{selectedCampaign, handleDelete, handlePageSelection}}/>}
+      {page === 'edit' && <Update {...{handleUpdate, selectedCampaign}}/>}
     </>
   )
 }
---
+++ frontend/src/services/campaignService.js
@@ -0,0 +1,49 @@
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/pets`;
+
+const index = async () => {
+	try {
+		const res = await fetch(BASE_URL);
+		return res.json();
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+const create = async (campaign) => {
+	try {
+		const res = await fetch(BASE_URL, {
+			method: 'POST',
+			headers: {'Content-Type': 'application/json'},
+			body: JSON.stringify(campaign),
+		});
+		return res.json();
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+const updateCampaign = async (campaign, campainId) => {
+	try {
+		const res = await fetch(`${BASE_URL}/${campainId}`, {
+			method: 'PUT',
+			headers: {'Content-Type': 'application/json'},
+			body: JSON.stringify(campaign),
+		});
+		return res.json();
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+const deleteCampaign = async (campainId) => {
+	try {
+		const deletedPet = await fetch(`${BASE_URL}/${campainId}`, {
+			method: 'DELETE',
+			headers: {'Content-Type': 'application/json'},
+		});
+		return deletedPet;
+	} catch (err) {
+		console.log(err);
+	}
+}
+export { index, create, updateCampaign, deleteCampaign};
