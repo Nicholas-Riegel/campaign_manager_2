d30eeb2b409e3eab05fe4d62c90f354d0c10690f
Nicholas Riegel <nicholaspriegel@gmail.com>
2024-05-29 12:30:05 -0400

    added styling to campaign show page: still needs work

--- frontend/src/components/Campaigns/CampaignShow.jsx
+++ frontend/src/components/Campaigns/CampaignShow.jsx
@@ -1,4 +1,5 @@
 import { useNavigate, useParams } from "react-router-dom"
+import './CampaignShow.css'
 
 function CampaignShow({campaignsArray, charactersArray, handleDeleteCampaign}) {
 
@@ -17,19 +18,21 @@ function CampaignShow({campaignsArray, charactersArray, handleDeleteCampaign}) {
   const campaignCharacters = charactersArray.filter(character => selectedCampaign.campaignCharacters.includes(character._id))
 
   return (
-    <>
-      <h1>Campaign: {selectedCampaign.campaignName}</h1>
-      <h2>System: {selectedCampaign.campaignSystem}</h2>
-      <h2>Characters:</h2>
-        <ul>
-        {campaignCharacters.map((character, i) => (
-          <li key={i}>{character.characterName}</li>
-        ))}
-        </ul>
-      <button onClick={()=>handleDeleteCampaign(selectedCampaign._id)}>Delete</button>
-      <button onClick={()=>Navigate(`/campaign/${selectedCampaign._id}/edit`)}>Edit</button>
-    </>
+    <div id="campaign-show-wrapper">
+      <div id="campaign-show-div">
+        <h1>Campaign: {selectedCampaign.campaignName}</h1>
+        <h2>System: {selectedCampaign.campaignSystem}</h2>
+        <h2>Characters:</h2>
+          <ul>
+          {campaignCharacters.map((character, i) => (
+            <li key={i}>{character.characterName}</li>
+          ))}
+          </ul>
+        <button onClick={()=>handleDeleteCampaign(selectedCampaign._id)}>Delete</button>
+        <button onClick={()=>Navigate(`/campaign/${selectedCampaign._id}/edit`)}>Edit</button>
+      </div>
+    </div>
   )
 }
   
-  export default CampaignShow
+export default CampaignShow
