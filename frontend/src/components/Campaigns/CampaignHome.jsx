f65a17d743a11040576794b9c9e0a2e590ea8de0
Nicholas Riegel <nicholaspriegel@gmail.com>
2024-05-29 12:16:14 -0400

    added styling to campaign home page: still needs more work

--- frontend/src/components/Campaigns/CampaignHome.jsx
+++ frontend/src/components/Campaigns/CampaignHome.jsx
@@ -1,18 +1,20 @@
 import { useNavigate } from 'react-router-dom';
+import './CampaignHome.css'
 
 function CampaignHome({campaignsArray}) {
 
     const Navigate = useNavigate()
 
     return (
-      <>
-          <ul>
-              {campaignsArray.map((campaign, i)=>(
-                  <li key={i} onClick={()=>Navigate(`/campaign/${campaign._id}`)}>{campaign.campaignName}</li>
-              ))}
-          </ul>
-          <button onClick={()=>Navigate('/campaign/new')}>Create New Campaign</button>
-      </>
+      <div id='campaign-home-wrapper'>
+            <h1>Campaigns</h1>
+            <ul>
+                {campaignsArray.map((campaign, i)=>(
+                    <li key={i} onClick={()=>Navigate(`/campaign/${campaign._id}`)}>{campaign.campaignName}</li>
+                ))}
+            </ul>
+            <button onClick={()=>Navigate('/campaign/new')}>Create New Campaign</button>
+      </div>
     )
   }
   
