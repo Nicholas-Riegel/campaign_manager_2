dcdd0205a1d29e2f69f4fdfb5858a97743fee686
smkun <skunian@yahoo.com>
2024-05-30 10:10:30 -0400

    minor syntax

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,115 +1,179 @@
 // Import css for all components
-import './App.css'
+import "./App.css";
 
 // Import React tools
-import { useState, useEffect } from "react"
-import { Route, Routes } from "react-router-dom"
-import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 // Import services
-import * as campaignService from './services/campaignService'
-import * as characterService from './services/characterService'
+import * as campaignService from "./services/campaignService";
+import * as characterService from "./services/characterService";
 
 // Import components
-import NavBar from "./components/NavBar/NavBar"
-import HomePage from './components/Home/HomePage'
+import NavBar from "./components/NavBar/NavBar";
+import HomePage from "./components/Home/HomePage";
 
-import CampaignHome from './components/Campaigns/CampaignHome'
-import CampaignCreate from './components/Campaigns/CampaignCreate'
-import CampaignShow from './components/Campaigns/CampaignShow'
-import CampaignUpdate from "./components/Campaigns/CampaignUpdate"
+import CampaignHome from "./components/Campaigns/CampaignHome";
+import CampaignCreate from "./components/Campaigns/CampaignCreate";
+import CampaignShow from "./components/Campaigns/CampaignShow";
+import CampaignUpdate from "./components/Campaigns/CampaignUpdate";
 
-import CharacterHome from './components/Characters/CharacterHome'
-import CharacterCreate from './components/Characters/CharacterCreate'
-import CharacterShow from './components/Characters/CharacterShow'
-import CharacterUpdate from './components/Characters/CharacterUpdate'
+import CharacterHome from "./components/Characters/CharacterHome";
+import CharacterCreate from "./components/Characters/CharacterCreate";
+import CharacterShow from "./components/Characters/CharacterShow";
+import CharacterUpdate from "./components/Characters/CharacterUpdate";
 
 function App() {
+    const Navigate = useNavigate();
 
-  const Navigate = useNavigate()
+    const [campaignsArray, setCampaignsArray] = useState([]);
+    const [charactersArray, setCharactersArray] = useState([]);
 
-  const [campaignsArray, setCampaignsArray] = useState([])
-  const [charactersArray, setCharactersArray] = useState([])
+    // CAMPAIGN FUNCTIONS
 
-  // CAMPAIGN FUNCTIONS
+    const getAllCampaigns = async () => {
+        const allCampaigns = await campaignService.index();
+        setCampaignsArray(allCampaigns);
+    };
 
-  const getAllCampaigns = async () => {
-    const allCampaigns = await campaignService.index()
-    setCampaignsArray(allCampaigns)
-  }
+    const handleCreateCampaign = async (campaign) => {
+        await campaignService.create(campaign);
+        getAllCampaigns();
+        Navigate("/campaigns");
+    };
 
-  const handleCreateCampaign = async (campaign) => {
-    await campaignService.create(campaign)
-    getAllCampaigns()
-    Navigate('/campaigns')
-  }
-  
-  const handleDeleteCampaign = async (campaignId) => {
-    await campaignService.deleteCampaign(campaignId)
-    await getAllCampaigns()
-    Navigate('/campaigns')
-  }
-  
-  const handleUpdateCampaign = async (campaign) => {
-    await campaignService.updateCampaign(campaign, campaign._id)
-    await getAllCampaigns()
-    Navigate(`/campaign/${campaign._id}`)
-  }
-  
-  // CHARACTER FUNCTIONS
+    const handleDeleteCampaign = async (campaignId) => {
+        await campaignService.deleteCampaign(campaignId);
+        await getAllCampaigns();
+        Navigate("/campaigns");
+    };
 
-  const getAllCharacters = async () => {
-    const allCharacters = await characterService.index()
-    setCharactersArray(allCharacters)
-  }
+    const handleUpdateCampaign = async (campaign) => {
+        await campaignService.updateCampaign(campaign, campaign._id);
+        await getAllCampaigns();
+        Navigate(`/campaign/${campaign._id}`);
+    };
 
-  const handleCreateCharacter = async (character) => {
-    await characterService.create(character)
-    await getAllCharacters()
-    Navigate('/characters')
-  }
-  
-  const handleDeleteCharacter = async (characterId) => {
-    await characterService.deleteCharacter(characterId)
-    await getAllCharacters()
-    Navigate('/characters')
-  }
-  
-  const handleUpdateCharacter = async (character) => {
-    await characterService.updateCharacter(character, character._id)
-    await getAllCharacters()
-    Navigate(`/character/${character._id}`)
-  }
-  
-  // useEffect to get all campaigns and characters on page load
-  useEffect(()=>{
-    getAllCampaigns()
-    getAllCharacters()
-  }, [])
-  
-  return (
-    <>
-      <NavBar/>
-      <Routes>
+    // CHARACTER FUNCTIONS
 
-        {/* HOME ROUTE */}
-        <Route path='/' element={<HomePage/>}/>
+    const getAllCharacters = async () => {
+        const allCharacters = await characterService.index();
+        setCharactersArray(allCharacters);
+    };
 
-        {/* CAMPAIGN ROUTES */}
-        <Route path='/campaigns' element={<CampaignHome {...{campaignsArray}}/>}/>
-        <Route path='/campaign/new' element={<CampaignCreate {...{ charactersArray, handleCreateCampaign}}/>}/>
-        <Route path='/campaign/:campaignId' element={<CampaignShow {...{campaignsArray, charactersArray, handleDeleteCampaign}}/>}/>
-        <Route path='/campaign/:campaignId/edit' element={<CampaignUpdate {...{campaignsArray, charactersArray, handleUpdateCampaign}}/>}/>
-        
-        {/* CHARACTER ROUTES */}
-        <Route path='/characters' element={<CharacterHome {...{charactersArray}}/>}/>
-        <Route path='/character/new' element={<CharacterCreate {...{handleCreateCharacter, campaignsArray}}/>}/>
-        <Route path='/character/:characterId' element={<CharacterShow {...{charactersArray, campaignsArray, handleDeleteCharacter}}/>}/>
-        <Route path='/character/:characterId/edit' element={<CharacterUpdate {...{charactersArray, campaignsArray, handleUpdateCharacter}}/>}/>
+    const handleCreateCharacter = async (character) => {
+        await characterService.create(character);
+        await getAllCharacters();
+        Navigate("/characters");
+    };
 
-      </Routes>
-    </>
-  )
+    const handleDeleteCharacter = async (characterId) => {
+        await characterService.deleteCharacter(characterId);
+        await getAllCharacters();
+        Navigate("/characters");
+    };
+
+    const handleUpdateCharacter = async (character) => {
+        await characterService.updateCharacter(character, character._id);
+        await getAllCharacters();
+        Navigate(`/character/${character._id}`);
+    };
+
+    // useEffect to get all campaigns and characters on page load
+    useEffect(() => {
+        getAllCampaigns();
+        getAllCharacters();
+    }, []);
+
+    return (
+        <>
+            <NavBar />
+            <Routes>
+                {/* HOME ROUTE */}
+                <Route
+                    path="/"
+                    element={<HomePage />}
+                />
+
+                {/* CAMPAIGN ROUTES */}
+                <Route
+                    path="/campaigns"
+                    element={<CampaignHome {...{ campaignsArray }} />}
+                />
+                <Route
+                    path="/campaign/new"
+                    element={
+                        <CampaignCreate
+                            {...{ charactersArray, handleCreateCampaign }}
+                        />
+                    }
+                />
+                <Route
+                    path="/campaign/:campaignId"
+                    element={
+                        <CampaignShow
+                            {...{
+                                campaignsArray,
+                                charactersArray,
+                                handleDeleteCampaign,
+                            }}
+                        />
+                    }
+                />
+                <Route
+                    path="/campaign/:campaignId/edit"
+                    element={
+                        <CampaignUpdate
+                            {...{
+                                campaignsArray,
+                                charactersArray,
+                                handleUpdateCampaign,
+                            }}
+                        />
+                    }
+                />
+
+                {/* CHARACTER ROUTES */}
+                <Route
+                    path="/characters"
+                    element={<CharacterHome {...{ charactersArray }} />}
+                />
+                <Route
+                    path="/character/new"
+                    element={
+                        <CharacterCreate
+                            {...{ handleCreateCharacter, campaignsArray }}
+                        />
+                    }
+                />
+                <Route
+                    path="/character/:characterId"
+                    element={
+                        <CharacterShow
+                            {...{
+                                charactersArray,
+                                campaignsArray,
+                                handleDeleteCharacter,
+                            }}
+                        />
+                    }
+                />
+                <Route
+                    path="/character/:characterId/edit"
+                    element={
+                        <CharacterUpdate
+                            {...{
+                                charactersArray,
+                                campaignsArray,
+                                handleUpdateCharacter,
+                            }}
+                        />
+                    }
+                />
+            </Routes>
+        </>
+    );
 }
 
-export default App
+export default App;
